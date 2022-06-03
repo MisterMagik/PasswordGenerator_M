43e6f0329684a764e658ace8115100a327e704ac
MisterMagik <denis.panek2004@op.pl>
2022-06-03 07:53:55 +0200

    add main loop

--- PasswordRandom/PasswordRandom.c
+++ PasswordRandom/PasswordRandom.c
@@ -15,49 +15,53 @@ char passwordGenerated[MAX_LENGTH_PASSWORD];
 
 int main()
 {
-	printf("#######################################\n");
-	printf("# Welcome to password generator %s #\n", APP_VERSION);
-	printf("#######################################\n");
-
-	printf("Select option: \n");
-	printf("1. Generate new password\n");
-	printf("2. Exit\n");
-	scanf_s("%d", &chooseUser);
-
-	if(chooseUser == 1)
+	for (;;)
 	{
-		srand(time(NULL));
-		printf("Enter password length: ");
-		scanf_s("%d", &stringLen);
-		if (stringLen >= 516)
+		printf("\n#######################################\n");
+		printf("# Welcome to password generator %s #\n", APP_VERSION);
+		printf("#######################################\n");
+
+		printf("Select option: \n");
+		printf("1. Generate new password\n");
+		printf("2. Exit\n");
+		scanf_s("%d", &chooseUser);
+
+		if (chooseUser == 1)
+		{
+			srand(time(NULL));
+			printf("Enter password length: ");
+			scanf_s("%d", &stringLen);
+			if (stringLen >= 516)
+			{
+
+			}
+			for (int i = 0; i < stringLen; i++)
+			{
+
+				randomNumer[i] = rand() % 45;
+			}
+
+			printf("\n");
+			for (int y = 0; y < stringLen; y++)
+			{
+				int number;
+				number = randomNumer[y];
+				passwordGenerated[y] = hash(number);
+			}
+			printf("Generated password: ");
+			for (int a = 0; a < stringLen; a++)
+				printf("%c", passwordGenerated[a]);
+			printf("\nPress any key to continue");
+			getchar();
+			getchar();
+		}
+		else if (chooseUser == 2)
 		{
 
-		}
-		for (int i = 0; i < stringLen; i++)
-		{
-			
-			randomNumer[i] = rand() % 45;
+			break;
 		}
 	
-		printf("\n");
-		for (int y = 0; y < stringLen; y++)
-		{
-			int number;
-			number = randomNumer[y];
-			passwordGenerated[y] = hash(number);
-		}
-
-		for(int a = 0; a < stringLen; a++)
-		printf("%c", passwordGenerated[a]);
-		
-		
 	}
-	else if(chooseUser == 2)
-	{
-		
-		return 0;
-	}
-
 
 
 	return 0;
